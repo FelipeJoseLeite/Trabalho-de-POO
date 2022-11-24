c71f3d18b1164542099833cd13462a69ad19653a
FelipeJoseLeite <90483232+FelipeJoseLeite@users.noreply.github.com>
2022-11-24 01:49:24 -0300

    Add files via upload

---
+++ tabela banco.sql
@@ -0,0 +1,14 @@
+create table MODELO(id bigint auto_increment primary key,
+                    descricao varchar(200),
+                    ano int,
+                    cor_id int,
+                    foreign key (cor_id) references COR(id),
+                    portas int,
+                    combustivel varchar(50));
+
+create table MARCA(id bigint auto_increment primary key, 				  descricao varchar(100), 
+			  modelo_id int,
+			  foreign key (modelo_id) references MODELO(id));
+
+create table COR(id bigint auto_increment primary key,
+			 descricao varchar(200));
